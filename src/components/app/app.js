39624a7e1fe700f2712d7bc1dc8fa8a6fa1a6e02
Electr0n <hunterlzp@yandex.ru>
2017-12-08 23:05:40 +0300

    Header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../header/header';
 import './app.css';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app__container'>
+        <Header />
         <div className='background-gradient'>
           <div className='background-gradient__strip_first'></div>
           <div className='background-gradient__strip_second'></div>
