4c0db8b89df6771078fafe47e7796612286b0d54
Electr0n <hunterlzp@yandex.ru>
2017-12-08 22:05:50 +0300

    01 Main page background

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,8 +4,12 @@ import './app.css';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        MyToT
+      <div className='app__container'>
+        <div className='background-gradient'>
+          <div className='background-gradient__strip_first'></div>
+          <div className='background-gradient__strip_second'></div>
+          <div className='background-gradient__strip_third'></div>
+        </div>
       </div>
     );
   }
