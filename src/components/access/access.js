ec2249245aa7489315be0b52695b370908f31b76
Yauhen Skaradumau <y.skaradumau@godeltech.com>
2017-12-24 00:17:19 +0300

    Component access

---
+++ src/components/access/access.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './access.css';
+
+export default class Access extends Component{
+  render(){
+    return(
+      <div className='access'>
+        <div className='access__background'></div>
+        <div className='access__background-line'></div>
+        <div className='access__content'>
+          <div className='access__header'>Когда ваш ребенок попросит рассказать о своем детстве - просто дайте ему доступ к аккаунту</div>
+          <div className='access__text'>Бесценные моменты взросления Ваших детей в фото, видео и дневниках в нашем приложении.</div>
+          <div className='promo__side__download_link'></div>
+        </div>
+        <div className='access__image'></div>
+      </div>
+    );
+  }
+}
