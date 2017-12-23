641675e682e2c941ebec30e9065bb73bb67f551b
Yauhen Skaradumau <y.skaradumau@godeltech.com>
2017-12-24 02:46:36 +0300

    Animation to footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,7 +9,7 @@ export default class Footer extends Component{
         <div className='footer__logo'></div>
         <div className='footer__menu'>
           <div className='footer__menu__item'><a href='https://play.google.com/store/apps/details?id=com.inflow.mytot'>Скачать My Tot</a></div>
-          <div className='footer__menu__item'>Контакты</div>
+          <div className='footer__menu__item'><a href=''>Контакты</a></div>
         </div>
         <div className='footer__triangle'></div>
         <div className='footer__flag-stripped'></div>
