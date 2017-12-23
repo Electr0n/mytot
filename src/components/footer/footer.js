dd0eec8ba331a485c6051e4224042d5affa0eb73
Yauhen Skaradumau <y.skaradumau@godeltech.com>
2017-12-24 01:10:41 +0300

    Component footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+export default class Footer extends Component{
+  render(){
+    return(
+      <footer className='footer'>
+        <div className='footer__background'></div>
+        <div className='footer__logo'></div>
+        <div className='footer__menu'>
+          <div className='footer__menu__item'><a href=''>Скачать My Tot</a></div>
+          <div className='footer__menu__item'>Контакты</div>
+        </div>
+        <div className='footer__triangle'></div>
+        <div className='footer__flag-stripped'></div>
+      </footer>
+    );
+  }
+}
