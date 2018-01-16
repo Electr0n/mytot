5a1dde044a661275f0d0910e6766bbd0d7051717
Yauhen Skaradumau <y.skaradumau@godeltech.com>
2018-01-16 23:53:57 +0300

    Adaptive for share moments

--- src/components/common/arrow/arrow.js
+++ src/components/common/arrow/arrow.js
@@ -11,9 +11,13 @@ export default class Arrow extends Component{
     return this.props.flipped ? 'arrow arrow_flipped' : 'arrow';
   }
 
+  containerClassName(){
+    return this.props.flipped ? 'arrow__container arrow__container_flipped' : 'arrow__container'
+  }
+
   render(){
     return(
-      <div className='arrow__container'>
+      <div className={this.containerClassName()}>
         <div className={this.textClassName()}>Текст перехода</div>
         <div className={this.arrowClassName()}></div>
       </div>
